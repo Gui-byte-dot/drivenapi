3938ce529f5223ee3695e905aca993f92e116c5a
Gui-byte-dot <68041690+Gui-byte-dot@users.noreply.github.com>
2023-04-05 10:09:04 -0300

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const server = express();
 server.get('/health', (req,res) => {
   res.send('ok')
 })
+server.get('/calculator', convertCLTToPJ)
 server.listen(4000, () => {
   console.log('Tá executando...');
 })
